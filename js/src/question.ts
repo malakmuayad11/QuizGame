89d410f57ba5ef0607a1130abf63e0437dbcd020
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-05-13 18:18:27 +0300

    Added missed type annotations in the script file

--- js/src/question.ts
+++ js/src/question.ts
@@ -1,31 +1,68 @@
 "use strict";
 
+// export class Question {
+//   #_id = 0;
+//   #_q = "";
+//   #_answers = [];
+//   #_correctAnswer = 0;
+
+//   constructor(id, q, answers, correctAnswer) {
+//     this.#_id = id;
+//     this.#_q = q;
+//     this.#_answers = answers;
+//     this.#_correctAnswer = answers[correctAnswer - 1];
+//   }
+//   get id() {
+//     return this.#_id;
+//   }
+//   get q() {
+//     return this.#_q;
+//   }
+//   get answers() {
+//     return this.#_answers;
+//   }
+//   get correctAnswer() {
+//     return this.#_correctAnswer;
+//   }
+
+//   isSelectedAnswerCorrect(currentQuestion) {
+//     return currentQuestion === this.#_correctAnswer;
+//   }
+// }
+
 export class Question {
-  #_id = 0;
-  #_q = "";
-  #_answers = [];
-  #_correctAnswer = 0;
+  public get correntAnswer(): number {
+    return this._correctAnswer;
+  }
+  public set correntAnswer(value: number) {
+    this._correctAnswer = value;
+  }
+  public get answers(): string[] {
+    return this._answers;
+  }
+  public set answers(value: string[]) {
+    this._answers = value;
+  }
+  public get q(): string {
+    return this._q;
+  }
+  public set q(value: string) {
+    this._q = value;
+  }
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
+  constructor(
+    private _id: number,
+    private _q: string,
+    private _answers: string[],
+    private _correctAnswer: number,
+  ) {}
 
-  constructor(id, q, answers, correctAnswer) {
-    this.#_id = id;
-    this.#_q = q;
-    this.#_answers = answers;
-    this.#_correctAnswer = answers[correctAnswer - 1];
-  }
-  get id() {
-    return this.#_id;
-  }
-  get q() {
-    return this.#_q;
-  }
-  get answers() {
-    return this.#_answers;
-  }
-  get correctAnswer() {
-    return this.#_correctAnswer;
-  }
-
-  isSelectedAnswerCorrect(currentQuestion) {
-    return currentQuestion === this.#_correctAnswer;
+  public isSelectedAnswerCorrect(currentQuestion: number): boolean {
+    return currentQuestion === this._correctAnswer;
   }
 }
